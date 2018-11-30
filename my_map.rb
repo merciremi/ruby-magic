7fdb28a931a1974935a03707b4e6b5166226d476
Remi Mercier <remi.merci@gmail.com>
2018-11-30 15:59:32 +0100

    Correct typo

--- my_map.rb
+++ my_map.rb
@@ -1,14 +1,13 @@
-# Implement my own .map method
 class Array
+  # Implement my own .map method: simple version
   def my_map
     results = []
     self.each { |integer| result << yield(integer) }
     results
   end
 end
+
 # .map will return a new array with each integer multiply by two
 p [1, 2, 3].map { |integer| integer * 2 }
 # .my_map should return the same
-p [1, 2, 3].my_map! { |integer| integer * 2 }
-
-
+p [1, 2, 3].my_map { |integer| integer * 2 }
